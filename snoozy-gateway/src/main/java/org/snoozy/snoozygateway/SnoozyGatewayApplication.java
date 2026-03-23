a71e901d574524ebc3164313233f2a889e3dc245
fakechitor <ladasedanbaklajan06@gmail.com>
2026-03-23 12:12:56 +0300

    build(gateway): init basic gateway

---
+++ snoozy-gateway/src/main/java/org/snoozy/snoozygateway/SnoozyGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.snoozy.snoozygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnoozyGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnoozyGatewayApplication.class, args);
+    }
+
+}
