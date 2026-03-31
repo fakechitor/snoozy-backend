3b296f6b5c3b08379679ee2ce8d7f2b7c8bfea27
fakechitor <ladasedanbaklajan06@gmail.com>
2026-03-31 18:21:48 +0300

    build(file): init basic project for file service

---
+++ snoozy-file/src/main/java/org/snoozy/snoozyfile/SnoozyFileApplication.java
@@ -0,0 +1,13 @@
+package org.snoozy.snoozyfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnoozyFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnoozyFileApplication.class, args);
+    }
+
+}
