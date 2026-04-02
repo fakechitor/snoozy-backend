20e3336eb70e3a2c8e73b5ee62ddfcacd048dc1e
fakechitor <ladasedanbaklajan06@gmail.com>
2026-04-02 17:44:20 +0300

    feat(group): init project for group service

---
+++ snoozy-group/src/main/java/org/snoozy/snoozygroup/SnoozyGroupApplication.java
@@ -0,0 +1,13 @@
+package org.snoozy.snoozygroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnoozyGroupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnoozyGroupApplication.class, args);
+    }
+
+}
---
+++ snoozy-group/src/test/java/org/snoozy/snoozygroup/SnoozyGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package org.snoozy.snoozygroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnoozyGroupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
