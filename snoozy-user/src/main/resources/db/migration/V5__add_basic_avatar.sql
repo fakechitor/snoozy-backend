a31df9659831262e02cc0822122ffb90dc8e13a6
fakechitor <ladasedanbaklajan06@gmail.com>
2026-04-02 14:45:34 +0300

    feat(user): add migration for basic avatar

---
+++ snoozy-user/src/main/resources/db/migration/V5__add_basic_avatar.sql
@@ -0,0 +1,6 @@
+INSERT INTO avatars(object_key, content_type, size_bytes)
+VALUES (
+           'https://sun9-18.userapi.com/s/v1/ig2/WssxJmCQhqXVZBBGTcLaQTxGphlPs846gAOjFcwq5A6aXYGe5lcXFA6sCSGiZswZTQW0BKcy6tOR-OmhWog1yc8T.jpg?quality=95&as=32x32,48x48,72x72,96x96&from=bu&u=ceXmspzLIAkkdVKk0ZHoLYQ53jdGBwYgy8DpZH6IRDg&cs=96x0',
+           'image/jpeg',
+           123
+       );
