d6349612a40ba50900f928a220568b9bfa38b369
fakechitor <ladasedanbaklajan06@gmail.com>
2026-04-05 19:12:48 +0300

    fix(users): fix avatarUrl in UserMapper

--- snoozy-user/src/main/java/org/snoozy/snoozyuser/dto/mapper/UserMapper.java
+++ snoozy-user/src/main/java/org/snoozy/snoozyuser/dto/mapper/UserMapper.java
@@ -1,17 +1,22 @@
 package org.snoozy.snoozyuser.dto.mapper;
 
+import org.snoozy.snoozyuser.dto.AvatarResponseDto;
 import org.snoozy.snoozyuser.dto.UserResponseDto;
+import org.snoozy.snoozyuser.model.Avatar;
 import org.snoozy.snoozyuser.model.User;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserMapper {
     public UserResponseDto getUserResponseDto(User user) {
+        Avatar avatar = user.getAvatar();
+        String avatarUrl = avatar != null ? avatar.getObjectKey() : null;
+
         return new UserResponseDto(
                 user.getUsername(),
                 user.getEmail(),
                 user.getPhoneNumber(),
-                user.getAvatar().getObjectKey()
+                avatarUrl
         );
     }
 }
